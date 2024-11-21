138b5041dc022a645e937ce4b17a613e4b9acd7f
zyb <383080504@qq.com>
2024-11-21 11:15:42 +0800

    machine learning

--- ex6/test2.py
+++ ex6/test2.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from sklearn.svm import SVC
 import scipy.io as sio
 
-#                      线性不可分svm
+#                       线性不可分svm
 
 path = "E:/BaiduNetdiskDownload/data_sets/ex6data2.mat"
 data = sio.loadmat(path)
---
+++ ex6/test3.py
@@ -0,0 +1,5 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.svm import SVC
+import scipy.io as sio
